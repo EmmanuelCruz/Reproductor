b6b365870860a246fdae768f8f69d5c8a08869d8
boruroad <adrianbonilla@ciencias.unam.mx>
2020-07-20 18:28:00 -0500

    Implementacion clase playlist

--- src/Reproductor/Playlist.java
+++ src/Reproductor/Playlist.java
@@ -5,11 +5,63 @@
  */
 package Reproductor;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author Emmanuel CRuz Hernández
- * @author Adrián Bonilla Ruiz 
+ * @author Emmanuel Cruz Hernández
+ * @author Adrián Bonilla Ruiz
+ * @author Cristobal Morales Karen
  */
 public class Playlist {
     
+    /** Nombre de la playlist */
+    public String nombre;
+    
+    /** Numero de canciones de la playlist */
+    public int numeroCanciones;
+    
+    /** Lista de canciones que guarda la playlist */
+    public ArrayList<Cancion> lista; 
+    
+    /**
+     * Construye una nueva playlist. 
+     * @param nombre el nombre de la playlist.
+     */
+    
+    public Playlist(String nombre ){
+        this.nombre = nombre;
+        lista = new ArrayList<>();  
+    }
+    
+    /**
+     * Agrega una cancion a la playlist.
+     */
+    public void agrega(Cancion cancion){
+        lista.add(cancion);
+        numeroCanciones++;
+        
+    }
+    
+    /**
+     * Elimina una cancion a la playlist.
+     */
+    public void elimina(Cancion cancion){
+        lista.remove(cancion);
+        numeroCanciones--;
+    }
+    
+    /**
+     * Agrega una cancion a la playlist.
+     */
+    public void Limpiar(){
+        lista.clear();
+        numeroCanciones = 0;
+    }
+    
+    
+    
+    
+    
+            
 }
